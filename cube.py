f194fc85e4ddd7850321707be81aa31595903027
Alex Eidt <alex.eidt@outlook.com>
2022-11-15 10:25:19 -0800

    Removed numpy and cv2 dependencies

--- cube.py
+++ cube.py
@@ -1,7 +1,6 @@
+import math
 import tkinter as tk
-import numpy as np
-import cv2
-from PIL import Image, ImageTk
+from PIL import Image, ImageDraw, ImageTk
 from linalg import Vector, Matrix
 
 WIDTH = 640
@@ -14,24 +13,37 @@ def main():
     root.title("Cube")
     root.resizable(width=False, height=False)
 
-    image = np.zeros((HEIGHT, WIDTH, 3), dtype=np.uint8)
-    label = tk.Label(root, image=ImageTk.PhotoImage(image=Image.fromarray(image)))
+    image = Image.new('RGB', (WIDTH, HEIGHT))
+    label = tk.Label(root)
 
     points = []
     for i in range(8):
         s = format(i, "03b")  # Zero-padded binary representation of i.
         points.append(Vector([float(d) - 0.5 for d in s]))
 
+    draw = ImageDraw.Draw(image)
+
+    stop = []
+    root.protocol("WM_DELETE_WINDOW", lambda: stop.append(0))
+
     angle = 0.0
-    while True:
-        cube(points, image, label, angle)
+    while not stop:
+        draw_cube(points, draw, angle)
+
+        # Update screen.
+        ph_image = ImageTk.PhotoImage(image)
+        label.configure(image=ph_image)
+        label.image = ph_image
+        label.pack()
+
         root.update_idletasks()
         root.update()
+
         angle += 0.004
 
 
-def cube(points, image, label, angle):
-    cos, sin = np.cos(angle), np.sin(angle)
+def draw_cube(points, image, angle):
+    cos, sin = math.cos(angle), math.sin(angle)
     rx = Matrix([[1, 0, 0], [0, cos, -sin], [0, sin, cos]])
     ry = Matrix([[cos, 0, sin], [0, 1, 0], [-sin, 0, cos]])
     rz = Matrix([[cos, -sin, 0], [sin, cos, 0], [0, 0, 1]])
@@ -45,7 +57,7 @@ def cube(points, image, label, angle):
         projection = Matrix([[z, 0, 0], [0, z, 0]])
         projected.append((projection * rotated) * (WIDTH / 2))
 
-    image[:] = 0  # Clear screen.
+    image.rectangle((0, 0, WIDTH, HEIGHT), 0) # Clear screen.
 
     for i in range(4):
         connect(projected[i], projected[i + 4], image)
@@ -57,19 +69,14 @@ def cube(points, image, label, angle):
         connect(projected[i], projected[i + 2], image)
         connect(projected[i + 4], projected[i + 6], image)
 
-    # Update screen.
-    frame_image = ImageTk.PhotoImage(image=Image.fromarray(image))
-    label.configure(image=frame_image)
-    label.image = frame_image
-    label.pack()
-
 
 def connect(v1, v2, image):
     x1, y1 = v1
     x2, y2 = v2
     x1, x2 = x1 + WIDTH / 2, x2 + WIDTH / 2
     y1, y2 = y1 + HEIGHT / 2, y2 + HEIGHT / 2
-    cv2.line(image, (int(x1), int(y1)), (int(x2), int(y2)), COLOR, THICKNESS)
+    
+    image.line((x1, y1, x2, y2), fill=COLOR, width=THICKNESS)
 
 
 if __name__ == "__main__":
--- tesseract.py
+++ tesseract.py
@@ -1,41 +1,52 @@
+import math
 import tkinter as tk
-import numpy as np
-import cv2
-from PIL import Image, ImageTk
+from PIL import Image, ImageDraw, ImageTk
 from linalg import Vector, Matrix
 
 WIDTH = 640
 HEIGHT = 360
 COLOR = (57, 255, 20)
-THICKNESS = 2
+THICKNESS = 3
 
 def main():
     root = tk.Tk()
     root.title("Tesseract")
     root.resizable(width=False, height=False)
 
-    image = np.zeros((HEIGHT, WIDTH, 3), dtype=np.uint8)
-    label = tk.Label(root, image=ImageTk.PhotoImage(image=Image.fromarray(image)))
+    image = Image.new('RGB', (WIDTH, HEIGHT))
+    label = tk.Label(root)
 
     points = []
     for i in range(16):
         s = format(i, "04b")  # Zero-padded binary representation of i.
         points.append(Vector([float(d) - 0.5 for d in s]))
 
+    draw = ImageDraw.Draw(image)
+
+    stop = []
+    root.protocol("WM_DELETE_WINDOW", lambda: stop.append(0))
+
     angle = 0.0
-    while True:
-        cube(points, image, label, angle)
+    while not stop:
+        draw_tesseract(points, draw, angle)
+
+        # Update screen.
+        ph_image = ImageTk.PhotoImage(image)
+        label.configure(image=ph_image)
+        label.image = ph_image
+        label.pack()
+
         root.update_idletasks()
         root.update()
 
-        angle += 0.004
+        angle += 0.002
 
 
-def cube(points, image, label, angle):
-    cos, sin = np.cos(angle), np.sin(angle)
+def draw_tesseract(points, image, angle):
+    cos, sin = math.cos(angle), math.sin(angle)
     rxy = Matrix([[cos, -sin, 0, 0], [sin, cos, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
     rzw = Matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, cos, -sin], [0, 0, sin, cos]])
-    rx = Matrix([[1, 0, 0], [0, 0, -1], [0, 1, 0]])  # PI / 2 Rotation about X-axis
+    rx = Matrix([[1, 0, 0], [0, 0, -1], [0, 1, 0]]) # PI / 2 Rotation about X-axis
 
     rotation = rxy * rzw
 
@@ -57,7 +68,7 @@ def cube(points, image, label, angle):
 
         projected.append(projected_2d * WIDTH)
 
-    image[:] = 0  # Clear screen.
+    image.rectangle((0, 0, WIDTH, HEIGHT), 0) # Clear screen.
 
     for i in range(4):
         connect(projected[i], projected[i + 4], image)
@@ -76,19 +87,14 @@ def cube(points, image, label, angle):
     for i in range(8):
         connect(projected[i], projected[i + 8], image)
 
-    # Update screen.
-    frame_image = ImageTk.PhotoImage(image=Image.fromarray(image))
-    label.configure(image=frame_image)
-    label.image = frame_image
-    label.pack()
-
 
 def connect(v1, v2, image):
     x1, y1 = v1
     x2, y2 = v2
     x1, x2 = x1 + WIDTH / 2, x2 + WIDTH / 2
     y1, y2 = y1 + HEIGHT / 2, y2 + HEIGHT / 2
-    cv2.line(image, (int(x1), int(y1)), (int(x2), int(y2)), COLOR, THICKNESS)
+    
+    image.line((x1, y1, x2, y2), fill=COLOR, width=THICKNESS)
 
 
 if __name__ == "__main__":
